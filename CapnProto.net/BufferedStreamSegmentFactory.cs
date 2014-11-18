dc7b3761ae320300e54afdab4aa67e346738a868
noel-honksw-com <noel@honksw.com>
2014-11-18 13:35:14 +0000

    Remove the Cache as non static and start to replace in various implementations. Breaking down the BufferedStreamFactory so can UT test it and implement the non static Cache implementation.

--- CapnProto.net/BufferedStreamSegmentFactory.cs
+++ CapnProto.net/BufferedStreamSegmentFactory.cs
@@ -2,6 +2,7 @@
 #define UNMANAGED
 #endif
 
+using CapnProto.Wrappers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,22 +14,120 @@ using System.Threading.Tasks;
 
 namespace CapnProto
 {
+    
+    /// <summary>
+    /// Need a separate builder/factory for creating a buffer segment factory. 
+    /// NOTE:: In reality see these being created from an IOC like TinyIOC....
+    /// </summary>
+    /// <remarks>
+    /// Wanted to remove the inbuilt dependency on the static Cache(T) class as it's not static anymore, also makes it easier to test.
+    /// Wanted to remove the responsibility of managing the caching behavior from the BufferSegmentFactory and have it placed a pipeline 
+    /// behavior. Means during disposal we can put in additional behaviors for things like Caching, again for testing reasons and also for single responsibility principals.
+    /// We can now inject the GC wrapper for testing.
+    /// </remarks>
+    class BufferedStreamSegmentFactoryBuilder
+    {
+        private readonly IGCWrapper gc;
+        private Cache<BufferedStreamSegmentFactory> cache;
+        public BufferedStreamSegmentFactoryBuilder(IGCWrapper gc)
+        {
+            this.gc = gc;
+            cache = new Cache<BufferedStreamSegmentFactory>(gc); 
+        }
+        public BufferedStreamSegmentFactory Create(Stream source, long length, bool leaveOpen)
+        {
+            var obj = cache.Pop() ?? new BufferedStreamSegmentFactory(DisposeAction, source, length, leaveOpen);
+            obj.Init(source, length, leaveOpen);
+            return obj;
+        }
+        /// <summary>
+        /// Manage caching internally in this class for the moment. Keeps the BufferedStreamSegmentFactory clean and testable.
+        /// </summary>
+        /// <param name="instance"></param>
+        private void DisposeAction(BufferedStreamSegmentFactory instance)
+        {
+            cache.Push(instance);
+        }
+    }
+
+    /// <summary>
+    /// Define a buffer accessor which is required for managed and unmanaged access in the BufferedStreamSegmentFactory.
+    /// </summary>
+    public interface IBufferProvider
+    {
+        byte[] Buffer { get; }
+    }
+    /// <summary>
+    /// The unmanaged provider also needs additional functionality for its behavior.
+    /// </summary>
+    public interface IUnmanagedBufferProvider : IBufferProvider
+    {
+        byte[] PopBuffer();
+        void PushBuffer(byte[] buffer);
+    }
+
+    /// <summary>
+    /// Implementation of the managed buffer provider 
+    /// </summary>
+    internal class ManagedBufferProvider : IBufferProvider
+    {
+        public ManagedBufferProvider(int bufferSize)
+        {
+            Buffer = new byte[bufferSize];
+        }
+        public byte[] Buffer {get; private set;}
+    }
+
+    /// <summary>
+    /// Moved the implementation of the Unmanaged static buffer to this implementation.
+    /// </summary>
+    /// <remarks>
+    /// Remove some convoluted code from the BufferedStreamSegmentFactory and allows us to easily change the
+    /// buffering mechanism.
+    /// </remarks>
+    internal class UnManagedBufferProvider : IUnmanagedBufferProvider
+    {
+        //TODO:: Clarify with Marc if those should be ThreadStatic
+        private static byte[] sharedBuffer;
+        public UnManagedBufferProvider(int messageWordLength)
+        {
+            Buffer = sharedBuffer;
+        }
+        public byte[] Buffer { get; private set; }
+        public byte[] PopBuffer()
+        {
+            return Interlocked.Exchange(ref sharedBuffer, null) ?? new byte[1024 * Message.WordLength];
+        }
+        public void PushBuffer(byte[] buffer)
+        {
+            if (buffer != null) Interlocked.Exchange(ref sharedBuffer, buffer);
+        }
+    }
+
     class BufferedStreamSegmentFactory : SegmentFactory
     {
         Stream source;
         long lastWord, remainingWords;
         bool leaveOpen;
         readonly byte[] scratch = new byte[8];
+        readonly Action<BufferedStreamSegmentFactory> disposeAction;
+//Depending on implementation required define the buffer provider.
+#if UNMANAGED
+        IUnmanagedBufferProvider bufferProvider;
+#else
+        IBufferProvider bufferProvider;
+#endif
 
-        public static BufferedStreamSegmentFactory Create(Stream source, long length, bool leaveOpen)
+        public BufferedStreamSegmentFactory(Action<BufferedStreamSegmentFactory> disposeAction, Stream source, long length, bool leaveOpen)
         {
-            var obj = Cache<BufferedStreamSegmentFactory>.Pop() ?? new BufferedStreamSegmentFactory();
-            obj.Init(source, length, leaveOpen);
-            return obj;
+            this.disposeAction = disposeAction;
+            Init(source, length, leaveOpen);
         }
         public override void Dispose()
         {
-            Cache<BufferedStreamSegmentFactory>.Push(this);
+            //TODO: Clarify with Marc if his intent here was to call the base.Dispose() I put this in, Was missing from initial implementation.
+            base.Dispose();
+            disposeAction(this);
         }
         protected override void Reset(bool recyclying)
         {
@@ -41,7 +140,7 @@ namespace CapnProto
             leaveOpen = false;
             base.Reset(recyclying);
         }
-        private void Init(Stream source, long length, bool leaveOpen)
+        internal void Init(Stream source, long length, bool leaveOpen)
         {
             this.source = source;
             this.lastWord = 0;
@@ -73,7 +172,7 @@ namespace CapnProto
             value = BitConverter.ToUInt64(scratch, 0);
             return true;
         }
-        static bool Read(Stream source, byte[] buffer, int count)
+        bool Read(Stream source, byte[] buffer, int count)
         {
             int offset = 0, read;
             while (count > 0 && (read = source.Read(buffer, offset, count)) > 0)
@@ -93,15 +192,6 @@ namespace CapnProto
 #endif
         }
 
-        static byte[] sharedBuffer;
-        private static byte[] PopBuffer()
-        {
-            return Interlocked.Exchange(ref sharedBuffer, null) ?? new byte[1024 * Message.WordLength];
-        }
-        private static void PushBuffer(byte[] buffer)
-        {
-            if(buffer != null) Interlocked.Exchange(ref sharedBuffer, buffer);
-        }
         protected override bool InitializeSegment(ISegment segment, long wordOffset, int totalWords, int activeWords)
         {
             CheckLastWord(wordOffset, totalWords);
@@ -112,12 +202,13 @@ namespace CapnProto
             }
 
 #if UNMANAGED
+            bufferProvider = new UnManagedBufferProvider(Message.WordLength);
             var ptr = default(IntPtr);
             byte[] buffer = null;
             try {
                 long bytes = ((long)totalWords) << 3;
                 ptr = Marshal.AllocHGlobal(totalWords << 3);
-                buffer = PopBuffer();
+                buffer = bufferProvider.PopBuffer();
 
                 IntPtr writeHead = ptr;
                 while(bytes > 0)
@@ -133,20 +224,19 @@ namespace CapnProto
             }
             finally
             {
-                PushBuffer(buffer);
+                bufferProvider.PushBuffer(buffer);
                 if (ptr != default(IntPtr)) Marshal.FreeHGlobal(ptr);
             }
 #else
-            byte[] buffer = new byte[totalWords << 3];
-            if (!Read(source, buffer, buffer.Length))
+            bufferProvider = new ManagedBufferProvider(totalWords << 3);
+            if (!Read(source, bufferProvider.Buffer, bufferProvider.Buffer.Length))
             {
                 return false;
             }
-            ((BufferSegment)segment).Init(buffer, 0, totalWords, activeWords);
+            ((BufferSegment)segment).Init(bufferProvider.Buffer, 0, totalWords, activeWords);
 #endif
             if (this.remainingWords > 0) this.remainingWords -= totalWords;
             return true;
         }
-        
     }
 }
